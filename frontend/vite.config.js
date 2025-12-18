7dceedc71f2087135055d248de6b02db5bc13564
Lejo-C <lejo3027@gmail.com>
2025-12-18 23:26:15 +0530

    message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,20 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      API: JSON.stringify(isProd ? 'https://deadline-saver-mvp.onrender.com' : 'http://localhost:5000')
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 })
